a45bfe56746b62878dcd079c432f84ef2a9f5a11
simonmullaney <simonpmullaney@gmail.com>
2018-03-26 17:12:52 +0100

    Some prints added

--- contracts/IPFS.sol
+++ contracts/IPFS.sol
@@ -10,11 +10,11 @@ contract ipfs is usingOraclize {
 
   //constructor put printed line from ethereum bridge in here!
   function ipfs(){
-      OAR = OraclizeAddrResolverI(0x6D749748DB7e336bF4423fDc19a29bCA27bd3751);
+      OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
   }
   //QmautuU5VBF9KgYvhM5eB4VkXjMURXKVGEZWWWCcUxfVHy is the hash of 'Ethereum Meetup 28 march\n' file stored on IPFS
   function getIPFSData() {
-      oraclize_query("IPFS", "QmautuU5VBF9KgYvhM5eB4VkXjMURXKVGEZWWWCcUxfVHy");
+      oraclize_query("IPFS", "Qmc2hpZFu3egFBCZcm2JXrPTMMbY4kpRHgPVnqxtEq9neW");
   }
 
   function __callback(bytes32 myid, string result) {
--- test/test.js
+++ test/test.js
@@ -2,13 +2,14 @@ var ipfs = artifacts.require("./IPFS.sol")
 
 contract('ipfs', function(accounts) {
 
-  let Simon = '0x9e575aef89cffe9db726012415fc02f6a5c2bfa0';
+  let Simon = '0xd0370b74cf2faba10a6badba5a976a66a3292131';
   var instance
 
   it("should send ether to contract adress so we can use the orcalize service",function(){
       var lease
       return ipfs.deployed()
       .then(function(instance) {
+          console.log("Contract address: " + instance.address);
           //Send eth from my account (First account given in testrpc) to the contract adress
           return web3.eth.sendTransaction({from: Simon, to: instance.address, value: web3.toWei(1,'ether'), gas: 4000000});
       })
@@ -28,7 +29,7 @@ it("should retrive file from IPFS", function () {
         //call results variable that the result of the oraclize service was stored
         return contract.results.call()
     }).then(function(res){
-        return assert.equal('Ethereum Meetup 28 march\n', res, "not working!!")
+        return assert.equal('Ethereum Meetup 28 march.\n', res, "not working!!")
     })
   })
 })
